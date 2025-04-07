36de6fb2d96ccf6786c37762c62c41134a472249
thaohuynh <huynhngocthao989@gmail.com>
2025-04-07 17:26:23 +0700

    Hook and flow

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,14 +1,33 @@
 import { test } from "@playwright/test";
 
-test("the first test", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
-  await page.getByText("Forms").click();
 });
 
-test.describe("the first test suite", () => {
-  test("the first test", () => {});
+test.describe("suite1", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Charts").click();
+  });
 
-  test("the second test", () => {});
+  test("the first test", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
 
-  test("the third test", () => {});
+  test.skip("navigate to datepicker page", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
+});
+
+test.describe.only("suite2", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Formsc").click();
+  });
+
+  test("the first test", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("navigate to datepicker page", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
