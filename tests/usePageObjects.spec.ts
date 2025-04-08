8df3570fd355fc553da456ec7960427c596f67f9
thaohuynh <huynhngocthao989@gmail.com>
2025-04-08 22:06:42 +0700

    Parameterized methods and documented the test methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { NavigationPage } from "../page_objects/navigationPage";
+import { FormLayoutsPage } from "../page_objects/formLayoutsPage";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
@@ -12,4 +13,22 @@ test("navigate to form page", async ({ page }) => {
   await navigation.smartTablePage();
   await navigation.toastrPage();
   await navigation.tooltipPage();
+});
+
+test("parameterized methods", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  const onFormLayoutsPage = new FormLayoutsPage(page);
+
+  await navigateTo.formLayoutPage();
+  await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption(
+    "test@test.com",
+    "Welcome1",
+    "Option 2"
+  );
+
+  await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(
+    "John Smith",
+    "john@example.com",
+    true
+  );
 });
